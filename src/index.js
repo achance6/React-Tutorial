e7d7a32956a987af66f13d1a84ddf7aaa9840ec6
Ayden Chance <achance6@jhu.edu>
2022-10-14 14:05:46 -0400

    comments

--- src/index.js
+++ src/index.js
@@ -48,6 +48,7 @@ class Board extends React.Component {
 class Game extends React.Component {
     constructor(props) {
         super(props);
+        // array of array of squares
         this.state = {
             history: [{ 
                 squares: Array(9).fill(null), 
@@ -58,7 +59,9 @@ class Game extends React.Component {
     }
 
     handleClick(i) {
-        const history = this.state.history.slice(0, this.state.stepNumber + 1); // Throw future "history" away if move made in past
+        // Throw future "history" away if move made while looking at past board
+        // Creating copies of these arrays instead of mutating them to maintain immutability for react
+        const history = this.state.history.slice(0, this.state.stepNumber + 1); 
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
