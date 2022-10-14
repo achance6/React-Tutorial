ad56753082f007cc1eab7786f4ae81672d496a48
Ayden Chance <achance6@jhu.edu>
2022-10-13 20:42:02 -0400

    fully implemented 'time-travel'

--- src/index.js
+++ src/index.js
@@ -53,11 +53,12 @@ class Game extends React.Component {
                 squares: Array(9).fill(null), 
             }],
             xIsNext: true,
+            stepNumber: 0,
         };
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1); // Throw future "history" away if move made in past
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -68,13 +69,22 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    // Don't need to update history as unspecified props in state are left as after setState
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
         });
     }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
         const moves = history.map((_step, move) => {
